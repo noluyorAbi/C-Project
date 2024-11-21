8d505bd32ee1eaaf7046c8a6342d2edac35d34cf
mvritz <moritz.dresch@titanom.com>
2024-11-21 15:58:26 +0100

    fix: clang-format

--- neunermuehle-client/main.c
+++ neunermuehle-client/main.c
@@ -2,13 +2,13 @@
 
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
+int main (int argc, char *argv[]) {
   GameConfig config;
-  if (!parse_args(argc, argv, &config)) {
-    print_usage(argv[0]);
+  if (!parse_args (argc, argv, &config)) {
+    print_usage (argv[0]);
     return 1;
   }
-  printf("GAME-ID: %s\n", config.game_id);
-  printf("Spielerzahl: %d\n", config.player_number);
+  printf ("GAME-ID: %s\n", config.game_id);
+  printf ("Spielerzahl: %d\n", config.player_number);
   return 0;
 }

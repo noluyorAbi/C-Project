8a34c21dfcaf7779a363c51f40de965db9fd4f2e
Jonas Peter <j.peter0308@gmail.com>
2024-11-22 19:43:00 +0100

    reformatted performConnection.c & .h with clang

--- modules/tcp_performConnection/performConnection.h
+++ modules/tcp_performConnection/performConnection.h
@@ -3,7 +3,7 @@
 
 /**
  * @brief Handles the Prolog phase of the communication protocol.
- * 
+ *
  * @param sockfd The socket file descriptor for the TCP connection.
  * @return int 0 on success, -1 on error.
  */

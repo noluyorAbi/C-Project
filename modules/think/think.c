bc5e30cbab64827f6b1eeed140d9199aa639ad55
Adatepe, Alperen <121226886+noluyorAbi@users.noreply.github.com>
2025-01-06 18:03:23 +0100

    Refactors think function to improve game state logging and remove commented debug output

--- modules/think/think.c
+++ modules/think/think.c
@@ -10,7 +10,7 @@ int think(char *gameState) {
   for (int i = 0; i < 25; i++) {
     board[i] = '+'; // Replace 42 with your desired value
   }
-  printf("Thinker: TEST Received game state:\n%s\n", gameState);
+  printf("\nThinker: Received game state:\n");
 
   // Array zur Speicherung der Positionen
   char positions[MAX_PIECES][10]; // Angenommen: Maximal 100 Positionen, jede
@@ -37,138 +37,138 @@ int think(char *gameState) {
     // Nächste Zeile
     line = strtok(NULL, "\n");
   }
-
-  // Ausgabe der gespeicherten Positionen
-  printf("Stored positions:\n");
-  for (int i = 0; i < pieceCount; i++) {
-    printf("Position %c: %s\n", i, positions[i]);
-  }
-
+  /*
+    // Ausgabe der gespeicherten Positionen
+    // printf("Stored positions:\n");
+    for (int i = 0; i < pieceCount; i++) {
+      // printf("Position %c: %s\n", i, positions[i]);
+    }
+   */
   // Switch case um Steine zu printen
   for (int i = 0; i < pieceCount; i++) {
     size_t len = strlen(positions[i]);
     if (len >= 2) {
       switch (positions[i][len - 6]) {
       case '0':
-        printf("Der Buchstabe im String '%s' ist '0'\n", positions[i]);
+        // printf("Der Buchstabe im String '%s' ist '0'\n", positions[i]);
         switch (positions[i][len - 2]) {
         case 'A':
-          printf("Der Buchstabe im String '%s' ist 'A'\n", positions[i]);
+          // printf("Der Buchstabe im String '%s' ist 'A'\n", positions[i]);
           switch (positions[i][len - 1]) {
           case '0':
-            printf("Wert ist A0\n");
+            // printf("Wert ist A0\n");
             board[0] = 'O';
             break;
           case '1':
-            printf("Wert ist A1\n");
+            // printf("Wert ist A1\n");
             board[1] = 'O';
             break;
           case '2':
-            printf("Wert ist A2\n");
+            // printf("Wert ist A2\n");
             board[2] = 'O';
             break;
           case '3':
-            printf("Wert ist A3\n");
+            // printf("Wert ist A3\n");
             board[14] = 'O';
             break;
           case '4':
-            printf("Wert ist A4\n");
+            // printf("Wert ist A4\n");
             board[23] = 'O';
             break;
           case '5':
-            printf("Wert ist A5\n");
+            // printf("Wert ist A5\n");
             board[22] = 'O';
             break;
           case '6':
-            printf("Wert ist A6\n");
+            // printf("Wert ist A6\n");
             board[21] = 'O';
             break;
           case '7':
-            printf("Wert ist A7\n");
+            // printf("Wert ist A7\n");
             board[9] = 'O';
             break;
           default:
-            printf("Wert ist nicht zwischen 0 und 7\n");
+            // printf("Wert ist nicht zwischen 0 und 7\n");
             break;
           }
           break;
         case 'B':
-          printf("Der Buchstabe im String '%s' ist 'B'\n", positions[i]);
+          // printf("Der Buchstabe im String '%s' ist 'B'\n", positions[i]);
           switch (positions[i][len - 1]) {
           case '0':
-            printf("Wert ist B0\n");
+            // printf("Wert ist B0\n");
             board[3] = 'O';
             break;
           case '1':
-            printf("Wert ist B1\n");
+            // printf("Wert ist B1\n");
             board[4] = 'O';
             break;
           case '2':
-            printf("Wert ist B2\n");
+            // printf("Wert ist B2\n");
             board[5] = 'O';
             break;
           case '3':
-            printf("Wert ist B3\n");
+            // printf("Wert ist B3\n");
             board[13] = 'O';
             break;
           case '4':
-            printf("Wert ist B4\n");
+            // printf("Wert ist B4\n");
             board[20] = 'O';
             break;
           case '5':
-            printf("Wert ist B5\n");
+            // printf("Wert ist B5\n");
             board[19] = 'O';
             break;
           case '6':
-            printf("Wert ist B6\n");
+            // printf("Wert ist B6\n");
             board[18] = 'O';
             break;
           case '7':
-            printf("Wert ist B7\n");
+            // printf("Wert ist B7\n");
             board[10] = 'O';
             break;
           default:
-            printf("Wert ist nicht zwischen 0 und 7\n");
+            // printf("Wert ist nicht zwischen 0 und 7\n");
             break;
           }
           break;
         case 'C':
-          printf("Der Buchstabe im String '%s' ist 'C'\n", positions[i]);
+          // printf("Der Buchstabe im String '%s' ist 'C'\n", positions[i]);
           switch (positions[i][len - 1]) {
           case '0':
-            printf("Wert ist C0\n");
+            // printf("Wert ist C0\n");
             board[6] = 'O';
             break;
           case '1':
-            printf("Wert ist C1\n");
+            // printf("Wert ist C1\n");
             board[7] = 'O';
             break;
           case '2':
-            printf("Wert ist C2\n");
+            // printf("Wert ist C2\n");
             board[8] = 'O';
             break;
           case '3':
-            printf("Wert ist C3\n");
+            // printf("Wert ist C3\n");
             board[12] = 'O';
             break;
           case '4':
-            printf("Wert ist C4\n");
+            // printf("Wert ist C4\n");
             board[17] = 'O';
             break;
           case '5':
-            printf("Wert ist C5\n");
+            // printf("Wert ist C5\n");
             board[16] = 'O';
             break;
           case '6':
-            printf("Wert ist C6\n");
+            // printf("Wert ist C6\n");
             board[15] = 'O';
             break;
           case '7':
-            printf("Wert ist C7\n");
+            // printf("Wert ist C7\n");
             board[11] = 'O';
             break;
           default:
-            printf("Wert ist nicht zwischen 0 und 7\n");
+            // printf("Wert ist nicht zwischen 0 und 7\n");
             break;
           }
           break;
@@ -180,153 +180,154 @@ int think(char *gameState) {
         }
         break;
       case '1':
-        printf("Der Buchstabe im String '%s' ist '1'\n", positions[i]);
+        // printf("Der Buchstabe im String '%s' ist '1'\n", positions[i]);
         switch (positions[i][len - 2]) {
         case 'A':
-          printf("Der Buchstabe im String '%s' ist 'A'\n", positions[i]);
+          // printf("Der Buchstabe im String '%s' ist 'A'\n", positions[i]);
           switch (positions[i][len - 1]) {
           case '0':
-            printf("Wert ist A0\n");
+            // printf("Wert ist A0\n");
             board[0] = 'X';
+
             break;
           case '1':
-            printf("Wert ist A1\n");
+            // printf("Wert ist A1\n");
             board[1] = 'X';
 
             break;
           case '2':
-            printf("Wert ist A2\n");
+            // printf("Wert ist A2\n");
             board[2] = 'X';
 
             break;
           case '3':
-            printf("Wert ist A3\n");
+            // printf("Wert ist A3\n");
             board[14] = 'X';
 
             break;
           case '4':
-            printf("Wert ist A4\n");
+            // printf("Wert ist A4\n");
             board[23] = 'X';
 
             break;
           case '5':
-            printf("Wert ist A5\n");
+            // printf("Wert ist A5\n");
             board[22] = 'X';
 
             break;
           case '6':
-            printf("Wert ist A6\n");
+            // printf("Wert ist A6\n");
             board[21] = 'X';
 
             break;
           case '7':
-            printf("Wert ist A7\n");
+            // printf("Wert ist A7\n");
             board[9] = 'X';
 
             break;
           default:
-            printf("Wert ist nicht zwischen 0 und 7\n");
+            // printf("Wert ist nicht zwischen 0 und 7\n");
             break;
           }
           break;
         case 'B':
-          printf("Der Buchstabe im String '%s' ist 'B'\n", positions[i]);
+          // printf("Der Buchstabe im String '%s' ist 'B'\n", positions[i]);
           switch (positions[i][len - 1]) {
           case '0':
-            printf("Wert ist B0\n");
+            // printf("Wert ist B0\n");
             board[3] = 'X';
 
             break;
           case '1':
-            printf("Wert ist B1\n");
+            // printf("Wert ist B1\n");
             board[4] = 'X';
 
             break;
           case '2':
-            printf("Wert ist B2\n");
+            // printf("Wert ist B2\n");
             board[5] = 'X';
 
             break;
           case '3':
-            printf("Wert ist B3\n");
+            // printf("Wert ist B3\n");
             board[13] = 'X';
 
             break;
           case '4':
-            printf("Wert ist B4\n");
+            // printf("Wert ist B4\n");
             board[20] = 'X';
 
             break;
           case '5':
-            printf("Wert ist B5\n");
+            // printf("Wert ist B5\n");
             board[19] = 'X';
 
             break;
           case '6':
-            printf("Wert ist B6\n");
+            // printf("Wert ist B6\n");
             board[18] = 'X';
 
             break;
           case '7':
-            printf("Wert ist B7\n");
+            // printf("Wert ist B7\n");
             board[10] = 'X';
 
             break;
           default:
-            printf("Wert ist nicht zwischen 0 und 7\n");
+            // printf("Wert ist nicht zwischen 0 und 7\n");
             break;
           }
           break;
         case 'C':
-          printf("Der Buchstabe im String '%s' ist 'C'\n", positions[i]);
+          // printf("Der Buchstabe im String '%s' ist 'C'\n", positions[i]);
           switch (positions[i][len - 1]) {
           case '0':
-            printf("Wert ist C0\n");
+            // printf("Wert ist C0\n");
             board[6] = 'X';
 
             break;
           case '1':
-            printf("Wert ist C1\n");
+            // printf("Wert ist C1\n");
             board[7] = 'X';
 
             break;
           case '2':
-            printf("Wert ist C2\n");
+            // printf("Wert ist C2\n");
             board[8] = 'X';
 
             break;
           case '3':
-            printf("Wert ist C3\n");
+            // printf("Wert ist C3\n");
             board[12] = 'X';
 
             break;
           case '4':
-            printf("Wert ist C4\n");
+            // printf("Wert ist C4\n");
             board[17] = 'X';
 
             break;
           case '5':
-            printf("Wert ist C5\n");
+            // printf("Wert ist C5\n");
             board[16] = 'X';
 
             break;
           case '6':
-            printf("Wert ist C6\n");
+            // printf("Wert ist C6\n");
             board[15] = 'X';
 
             break;
           case '7':
-            printf("Wert ist C7\n");
+            // printf("Wert ist C7\n");
             board[11] = 'X';
 
             break;
           default:
-            printf("Wert ist nicht zwischen 0 und 7\n");
+            // printf("Wert ist nicht zwischen 0 und 7\n");
             break;
           }
           break;
         default:
-          printf("Der Buchstabe im String '%s' ist weder 'A', 'B' noch 'C' (er "
+          printf("Der Buchstabe im String '%s' ist weder 'A', 'B' noch 'C'(er "
                  "ist '%c')\n",
                  positions[i], positions[i][len - 2]);
           break;

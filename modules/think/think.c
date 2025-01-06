c6c55678923fd9d18062733ecf01416bd648a804
Adatepe, Alperen <121226886+noluyorAbi@users.noreply.github.com>
2025-01-06 17:23:08 +0100

    PROTOTYPE: mplements board representation from game state
    Adds a board array to store game state information
    
    Parses the received game state string and populates the board array based on piece positions
    Each piece is represented by a numeric value in the corresponding index of the board array
    Prints a visual representation of the board for debugging purposes

--- modules/think/think.c
+++ modules/think/think.c
@@ -6,6 +6,8 @@
 #define MAX_PIECES 18 // Maximale Anzahl an PIECEs
 
 int think(char *gameState) {
+  int board[25];
+
   printf("Thinker: TEST Received game state:\n%s\n", gameState);
 
   // Array zur Speicherung der Positionen
@@ -53,27 +55,35 @@ int think(char *gameState) {
           switch (positions[i][len - 1]) {
           case '0':
             printf("Wert ist A0\n");
+            board[0] = 0;
             break;
           case '1':
             printf("Wert ist A1\n");
+            board[1] = 0;
             break;
           case '2':
             printf("Wert ist A2\n");
+            board[2] = 0;
             break;
           case '3':
             printf("Wert ist A3\n");
+            board[14] = 0;
             break;
           case '4':
             printf("Wert ist A4\n");
+            board[23] = 0;
             break;
           case '5':
             printf("Wert ist A5\n");
+            board[22] = 0;
             break;
           case '6':
             printf("Wert ist A6\n");
+            board[21] = 0;
             break;
           case '7':
             printf("Wert ist A7\n");
+            board[9] = 0;
             break;
           default:
             printf("Wert ist nicht zwischen 0 und 7\n");
@@ -85,27 +95,35 @@ int think(char *gameState) {
           switch (positions[i][len - 1]) {
           case '0':
             printf("Wert ist B0\n");
+            board[3] = 0;
             break;
           case '1':
             printf("Wert ist B1\n");
+            board[4] = 0;
             break;
           case '2':
             printf("Wert ist B2\n");
+            board[5] = 0;
             break;
           case '3':
             printf("Wert ist B3\n");
+            board[13] = 0;
             break;
           case '4':
             printf("Wert ist B4\n");
+            board[20] = 0;
             break;
           case '5':
             printf("Wert ist B5\n");
+            board[19] = 0;
             break;
           case '6':
             printf("Wert ist B6\n");
+            board[18] = 0;
             break;
           case '7':
             printf("Wert ist B7\n");
+            board[10] = 0;
             break;
           default:
             printf("Wert ist nicht zwischen 0 und 7\n");
@@ -117,27 +135,35 @@ int think(char *gameState) {
           switch (positions[i][len - 1]) {
           case '0':
             printf("Wert ist C0\n");
+            board[6] = 0;
             break;
           case '1':
             printf("Wert ist C1\n");
+            board[7] = 0;
             break;
           case '2':
             printf("Wert ist C2\n");
+            board[8] = 0;
             break;
           case '3':
             printf("Wert ist C3\n");
+            board[12] = 0;
             break;
           case '4':
             printf("Wert ist C4\n");
+            board[17] = 0;
             break;
           case '5':
             printf("Wert ist C5\n");
+            board[16] = 0;
             break;
           case '6':
             printf("Wert ist C6\n");
+            board[15] = 0;
             break;
           case '7':
             printf("Wert ist C7\n");
+            board[11] = 0;
             break;
           default:
             printf("Wert ist nicht zwischen 0 und 7\n");
@@ -159,27 +185,42 @@ int think(char *gameState) {
           switch (positions[i][len - 1]) {
           case '0':
             printf("Wert ist A0\n");
+            board[0] = 1;
             break;
           case '1':
             printf("Wert ist A1\n");
+            board[1] = 1;
+
             break;
           case '2':
             printf("Wert ist A2\n");
+            board[2] = 1;
+
             break;
           case '3':
             printf("Wert ist A3\n");
+            board[14] = 1;
+
             break;
           case '4':
             printf("Wert ist A4\n");
+            board[23] = 1;
+
             break;
           case '5':
             printf("Wert ist A5\n");
+            board[22] = 1;
+
             break;
           case '6':
             printf("Wert ist A6\n");
+            board[21] = 1;
+
             break;
           case '7':
             printf("Wert ist A7\n");
+            board[9] = 1;
+
             break;
           default:
             printf("Wert ist nicht zwischen 0 und 7\n");
@@ -191,27 +232,43 @@ int think(char *gameState) {
           switch (positions[i][len - 1]) {
           case '0':
             printf("Wert ist B0\n");
+            board[3] = 1;
+
             break;
           case '1':
             printf("Wert ist B1\n");
+            board[4] = 1;
+
             break;
           case '2':
             printf("Wert ist B2\n");
+            board[5] = 1;
+
             break;
           case '3':
             printf("Wert ist B3\n");
+            board[13] = 1;
+
             break;
           case '4':
             printf("Wert ist B4\n");
+            board[20] = 1;
+
             break;
           case '5':
             printf("Wert ist B5\n");
+            board[19] = 1;
+
             break;
           case '6':
             printf("Wert ist B6\n");
+            board[18] = 1;
+
             break;
           case '7':
             printf("Wert ist B7\n");
+            board[10] = 1;
+
             break;
           default:
             printf("Wert ist nicht zwischen 0 und 7\n");
@@ -223,27 +280,43 @@ int think(char *gameState) {
           switch (positions[i][len - 1]) {
           case '0':
             printf("Wert ist C0\n");
+            board[6] = 1;
+
             break;
           case '1':
             printf("Wert ist C1\n");
+            board[7] = 1;
+
             break;
           case '2':
             printf("Wert ist C2\n");
+            board[8] = 1;
+
             break;
           case '3':
             printf("Wert ist C3\n");
+            board[12] = 1;
+
             break;
           case '4':
             printf("Wert ist C4\n");
+            board[17] = 1;
+
             break;
           case '5':
             printf("Wert ist C5\n");
+            board[16] = 1;
+
             break;
           case '6':
             printf("Wert ist C6\n");
+            board[15] = 1;
+
             break;
           case '7':
             printf("Wert ist C7\n");
+            board[11] = 1;
+
             break;
           default:
             printf("Wert ist nicht zwischen 0 und 7\n");
@@ -268,6 +341,26 @@ int think(char *gameState) {
              positions[i]);
     }
   }
+
+  printf("\n"
+         " %c--------%c--------%c\n"
+         " |        |        |\n"
+         " |  %c-----5-----%c  |\n"
+         " |  |     |     |  |\n"
+         " |  |  %c--8--%c  |  |\n"
+         " |  |  |     |  |  |\n"
+         "%c-%c-%c    %c-%c-%c\n"
+         " |  |  |     |  |  |\n"
+         " |  |  %c--17--%c |\n"
+         " |  |     |     |  |\n"
+         " |  %c----20----%c |\n"
+         " |        |        |\n"
+         " %c-------%c-------%c\n"
+         "\n",
+         board[0], board[1], board[2], board[3], board[5], board[6], board[8],
+         board[9], board[10], board[11], board[12], board[13], board[14],
+         board[15], board[17], board[18], board[20], board[21], board[22],
+         board[23]);
 
   return EXIT_SUCCESS;
 }

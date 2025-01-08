7466e7ef9784b5c669b6f76dea78c011a3974cb7
Adatepe, Alperen <121226886+noluyorAbi@users.noreply.github.com>
2025-01-08 22:26:13 +0100

    refactor: remove redundant log for successful pipe read in gameplay.c

--- modules/tcp_performConnection/gameplay.c
+++ modules/tcp_performConnection/gameplay.c
@@ -74,9 +74,6 @@ int checkForSignal(int sockfd) {
       } else if (n == 0) {
         fprintf(stdout, "Thinker has closed the pipe.\n");
         return EXIT_FAILURE;
-      } else if (n == 1) {
-        fprintf(stdout, "Connector: Read %d bytes from the pipe sucessfully.\n",
-                n);
       }
 
       // Process the move sent by the Thinker
